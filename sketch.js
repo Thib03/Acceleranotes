6549c1c3f0976ad47d95c7fbfe6fe51eb480129b
Thib03 <41568449+Thib03@users.noreply.github.com>
2019-07-10 18:29:50 +0200

    now

--- sketch.js
+++ sketch.js
@@ -759,19 +759,7 @@ function preload()
   //soundFormats('mp3');
 
   //A5 = loadSound('A5');
-
-  console.log("yet");
 }
-
-WebMidi.enable(function (err) {
-  var input = WebMidi.inputs[0];
-
-  console.log(input);
-
-  input.addListener('noteon', "all", function(e) {
-    console.log("Pitch value: " + e.value);
-  });
-},true);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -836,6 +824,18 @@ function setup() {
   //text('ou',width/2,25.5*height/40);
 
   noLoop();
+
+  WebMidi.enable(function (err) {
+    var input = WebMidi.inputs[0];
+
+    console.log(input);
+
+    input.addListener('noteon', "all", function(e) {
+      console.log("Pitch value: " + e.value);
+    });
+  },true);
+
+  console.log("now");
 }
 
 function windowResized() {
